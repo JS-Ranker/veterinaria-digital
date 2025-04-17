f44c78ae1a926e858bb32f6823b5991c7e658b97
AlvaroWALOT <134461174+AlvaroWALOT@users.noreply.github.com>
2025-04-17 00:35:37 -0400

    copia src web
    se clona repositoria src web

---
+++ src_web/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// 🟣 Agregar ReactiveFormsModule
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    // 🟣 Agregar ReactiveFormsModule aquí
+    ReactiveFormsModule
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src_web/src/app/pages/equipo/equipo.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipo',
+  templateUrl: './equipo.page.html',
+  styleUrls: ['./equipo.page.scss'],
+  standalone: false
+})
+export class EquipoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
