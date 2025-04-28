b6e881ff54443087eb9b7c0f958912b86b26805c
JS-Ranker <hsanhueza.dev@gmail.com>
2025-04-28 00:37:23 -0400

    Delete BACKEND directory

--- BACKEND/src/routes/duenos.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDuenos, createDueno } = require('../controllers/duenos.controller');
-
-router.get('/', getDuenos);
-router.post('/', createDueno);
-
-module.exports = router;
